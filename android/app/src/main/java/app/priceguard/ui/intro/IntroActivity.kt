22a16bb707444769a6e53e90b34a7945d3a2c92b
ootr47 <ootr45@gmail.com>
2023-11-15 22:08:34 +0900

    feat: 이벤트 핸들러 -> 클릭 리스너로 변경

--- android/app/src/main/java/app/priceguard/ui/intro/IntroActivity.kt
+++ android/app/src/main/java/app/priceguard/ui/intro/IntroActivity.kt
@@ -2,44 +2,30 @@ package app.priceguard.ui.intro
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import app.priceguard.databinding.ActivityIntroBinding
-import app.priceguard.ui.intro.IntroViewModel.Event
 import app.priceguard.ui.login.LoginActivity
 import app.priceguard.ui.signup.SignupActivity
-import app.priceguard.ui.util.lifecycle.repeatOnStarted
 
 class IntroActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityIntroBinding
-    private val viewModel: IntroViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.viewModel = viewModel
-
-        repeatOnStarted {
-            viewModel.eventFlow.collect { event ->
-                handleEvent(event)
-            }
-        }
+        initListener()
     }
 
-    private fun handleEvent(event: Event) {
-        when (event) {
-            is Event.StartLoginActivity -> {
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-            }
-
-            is Event.StartSignupActivity -> {
-                val intent = Intent(this, SignupActivity::class.java)
-                startActivity(intent)
-            }
+    private fun initListener() {
+        binding.btnIntroLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnIntroSignUp.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
         }
     }
 }
