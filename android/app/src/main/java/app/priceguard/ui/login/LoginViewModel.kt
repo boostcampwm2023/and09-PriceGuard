cf28410cb2c8ccb0092ac7535bb7a7d4418aac81
EunhoKang <zay0njugin@gmail.com>
2023-11-15 22:44:28 +0900

    fix: import 제거

--- android/app/src/main/java/app/priceguard/ui/login/LoginViewModel.kt
+++ android/app/src/main/java/app/priceguard/ui/login/LoginViewModel.kt
@@ -1,14 +1,9 @@
 package app.priceguard.ui.login
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     data class State(
