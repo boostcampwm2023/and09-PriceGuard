0884511fd4c3a831bc0810a616eee2f8ab6fbc6b
EunhoKang <zay0njugin@gmail.com>
2023-11-15 22:15:24 +0900

    refactor: context 수정 및 binding 방식 변경

--- android/app/src/main/java/app/priceguard/ui/login/LoginActivity.kt
+++ android/app/src/main/java/app/priceguard/ui/login/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -16,13 +15,13 @@ import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
     private val loginViewModel: LoginViewModel by viewModels()
-    private var _binding: ActivityLoginBinding? = null
-    val binding: ActivityLoginBinding get() = _binding!!
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val context = this
         val loginFailDialog =
-            MaterialAlertDialogBuilder(this, R.style.ThemeOverlay_App_MaterialAlertDialog)
+            MaterialAlertDialogBuilder(context, R.style.ThemeOverlay_App_MaterialAlertDialog)
                 .setTitle("로그인 실패")
                 .setMessage("다시 시도해 주세요.")
                 .setPositiveButton("확인") { _, _ -> }
@@ -31,14 +30,20 @@ class LoginActivity : AppCompatActivity() {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 loginViewModel.event.collect {
                     when (it) {
-                        LoginViewModel.LoginEvent.GoToSignUp -> startActivity(Intent(baseContext, SignupActivity::class.java))
+                        LoginViewModel.LoginEvent.GoToSignUp -> startActivity(
+                            Intent(
+                                context,
+                                SignupActivity::class.java
+                            )
+                        )
+
                         LoginViewModel.LoginEvent.LoginFailed -> loginFailDialog.show()
                     }
                 }
             }
         }
 
-        _binding = DataBindingUtil.setContentView<ActivityLoginBinding>(this, R.layout.activity_login).apply {
+        binding = ActivityLoginBinding.inflate(layoutInflater).apply {
             viewModel = loginViewModel
             btnLoginLogin.setOnClickListener { loginViewModel.logIn() }
             btnLoginSignup.setOnClickListener { loginViewModel.signUp() }
