f4262eae02c3bcb9c8a946b417b13cfe7f6f6401
ootr47 <ootr45@gmail.com>
2023-11-15 15:43:46 +0900

    feat: 초기화면 화면 전환 이벤트 핸들러 추가

--- android/app/src/main/java/app/priceguard/ui/intro/IntroActivity.kt
+++ android/app/src/main/java/app/priceguard/ui/intro/IntroActivity.kt
@@ -1,11 +1,45 @@
 package app.priceguard.ui.intro
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import app.priceguard.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import app.priceguard.databinding.ActivityIntroBinding
+import app.priceguard.ui.intro.IntroViewModel.Event
+import app.priceguard.ui.login.LoginActivity
+import app.priceguard.ui.signup.SignupActivity
+import app.priceguard.ui.util.lifecycle.repeatOnStarted
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+    private val viewModel: IntroViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewModel = viewModel
+
+        repeatOnStarted {
+            viewModel.eventFlow.collect { event ->
+                handleEvent(event)
+            }
+        }
+    }
+
+    private fun handleEvent(event: Event) {
+        when (event) {
+            is Event.StartLoginActivity -> {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+
+            is Event.StartSignupActivity -> {
+                val intent = Intent(this, SignupActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
