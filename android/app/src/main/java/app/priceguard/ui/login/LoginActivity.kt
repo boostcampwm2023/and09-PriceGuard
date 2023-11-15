907cfee8f794b562e0ac6494ef060fd27792aec4
EunhoKang <zay0njugin@gmail.com>
2023-11-15 14:57:24 +0900

    feat: 로그인 Activity 수정 및 Intent 추가
    - inflate
    - viewmodel 생성 및 할당
    - 회원가입 버튼 클릭 시 회원가입 UI로 이동

--- android/app/src/main/java/app/priceguard/ui/login/LoginActivity.kt
+++ android/app/src/main/java/app/priceguard/ui/login/LoginActivity.kt
@@ -1,11 +1,39 @@
 package app.priceguard.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import app.priceguard.R
+import app.priceguard.databinding.ActivityLoginBinding
+import app.priceguard.ui.signup.SignupActivity
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
+    private val loginViewModel: LoginViewModel by viewModels()
+    private var _binding: ActivityLoginBinding? = null
+    val binding: ActivityLoginBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                loginViewModel.event.collect {
+                    when (it) {
+                        LoginViewModel.LoginEvent.SignUp -> startActivity(Intent(baseContext, SignupActivity::class.java))
+                    }
+                }
+            }
+        }
+
+        _binding = DataBindingUtil.setContentView<ActivityLoginBinding>(this, R.layout.activity_login).apply {
+            viewModel = loginViewModel
+        }
+        setContentView(binding.root)
     }
 }
