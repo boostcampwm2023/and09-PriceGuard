258ad73fbbb2291895ccb07bfcb3a3a976f73709
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-16 22:31:29 +0900

    feat: 비밀번호 비교 및 예외 처리

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/user.entity';
+import { ValidationException } from 'src/exceptions/validation.exception';
 
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService) {}
 
-    async validateUser(email: string, password: string): Promise<any> {
+    async validateUser(email: string, password: string): Promise<User> {
         const user = await this.usersService.findOne(email);
+        if (!user || !bcrypt.compareSync(password, user.password)) {
+            throw new ValidationException('로그인 실패');
+        }
+        return user;
     }
 }
