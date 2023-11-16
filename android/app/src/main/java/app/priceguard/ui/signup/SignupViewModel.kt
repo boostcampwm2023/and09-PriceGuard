a90d5cd37873aec9fdff4951f53eb0804435423f
Taewan Park <swimingtw@gmail.com>
2023-11-16 15:04:40 +0900

    chore: 코드 리포맷

--- android/app/src/main/java/app/priceguard/ui/signup/SignupViewModel.kt
+++ android/app/src/main/java/app/priceguard/ui/signup/SignupViewModel.kt
@@ -20,8 +20,10 @@ class SignupViewModel : ViewModel() {
         val isRetypePasswordError: Boolean? = null
     )
 
-    private val emailPattern = """^[\w.+-]+@((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$""".toRegex()
-    private val passwordPattern = """^(?=[A-Za-z\d!@#$%^&*]*\d)(?=[A-Za-z\d!@#$%^&*]*[a-z])(?=[A-Za-z\d!@#$%^&*]*[A-Z])(?=[A-Za-z\d!@#$%^&*]*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$""".toRegex()
+    private val emailPattern =
+        """^[\w.+-]+@((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$""".toRegex()
+    private val passwordPattern =
+        """^(?=[A-Za-z\d!@#$%^&*]*\d)(?=[A-Za-z\d!@#$%^&*]*[a-z])(?=[A-Za-z\d!@#$%^&*]*[A-Z])(?=[A-Za-z\d!@#$%^&*]*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$""".toRegex()
 
     private val _state: MutableStateFlow<SignupUIState> = MutableStateFlow(SignupUIState())
     val state: StateFlow<SignupUIState> = _state.asStateFlow()
