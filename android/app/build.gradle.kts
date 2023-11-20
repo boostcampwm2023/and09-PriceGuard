669feb0edfb6187653b62fdd283fcf3d0b1d85c1
EunhoKang <zay0njugin@gmail.com>
2023-11-20 22:38:10 +0900

    chore: navigation 의존성 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
     // Retrofit, Serialization
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
