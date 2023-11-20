6b7f18d834040334ce8c8e22203962273150a729
EunhoKang <zay0njugin@gmail.com>
2023-11-21 01:17:15 +0900

    feat: 상품 정보 개요 아이템 생성

---
+++ android/app/src/main/java/app/priceguard/ui/home/ProductSummary.kt
@@ -0,0 +1,13 @@
+package app.priceguard.ui.home
+
+enum class BrandType {
+    ELEVENST
+}
+
+data class ProductSummary(
+    val brandType: BrandType,
+    val title: String,
+    val price: String,
+    val discountPercent: String,
+    val isAlarmOn: Boolean
+)
