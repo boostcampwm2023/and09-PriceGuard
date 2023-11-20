6bbd480b2fd94fbba25ca6c70284d2f1e852b5d2
ootr47 <ootr45@gmail.com>
2023-11-21 02:37:02 +0900

    feat: 상품 추가 액티비티 구현, nav_graph 추가

---
+++ android/app/src/main/java/app/priceguard/ui/main/additem/AddItemActivity.kt
@@ -0,0 +1,20 @@
+package app.priceguard.ui.main.additem
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.priceguard.databinding.ActivityAddItemBinding
+
+class AddItemActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddItemBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAddItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mtAddItemTopbar.setNavigationOnClickListener {
+            finish()
+        }
+    }
+}
