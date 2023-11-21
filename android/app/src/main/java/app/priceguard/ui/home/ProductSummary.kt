e0520df00cab6af615f1dec810ea9b42ea55dc7c
EunhoKang <zay0njugin@gmail.com>
2023-11-22 01:18:22 +0900

    fix: sealed interface 구현부 수정

--- android/app/src/main/java/app/priceguard/ui/home/ProductSummary.kt
+++ android/app/src/main/java/app/priceguard/ui/home/ProductSummary.kt
@@ -1,40 +1,24 @@
 package app.priceguard.ui.home
 
 sealed interface ProductSummary {
-
-    fun readTitle(): String
-
-    fun readPrice(): String
-
-    fun readDiscountPercent(): String
+    val brandType: String
+    val title: String
+    val price: String
+    val discountPercent: String
 
     data class UserProductSummary(
-        val brandType: String,
-        val title: String,
-        val price: String,
-        val discountPercent: String,
+        override val brandType: String,
+        override val title: String,
+        override val price: String,
+        override val discountPercent: String,
         val isAlarmOn: Boolean
-    ) : ProductSummary {
-
-        override fun readTitle(): String = title
-
-        override fun readPrice(): String = price
-
-        override fun readDiscountPercent(): String = discountPercent
-    }
+    ) : ProductSummary
 
     data class RecommendedProductSummary(
-        val brandType: String,
-        val title: String,
-        val price: String,
-        val discountPercent: String,
+        override val brandType: String,
+        override val title: String,
+        override val price: String,
+        override val discountPercent: String,
         val recommendRank: Int
-    ) : ProductSummary {
-
-        override fun readTitle(): String = title
-
-        override fun readPrice(): String = price
-
-        override fun readDiscountPercent(): String = discountPercent
-    }
+    ) : ProductSummary
 }
