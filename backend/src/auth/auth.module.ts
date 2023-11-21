d9a13efa6d02c1e9293640a0463e517f7496c431
Muungi <mks4010@naver.com>
2023-11-21 09:47:30 +0900

    feat: refreshToken 인증, JWT 만료 별도 처리

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { AccessJwtStrategy } from './jwt/jwt.strategy';
+import { AccessJwtStrategy, RefreshJwtStrategy } from './jwt/jwt.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
     imports: [forwardRef(() => UsersModule), JwtModule.register({})],
-    providers: [AuthService, AccessJwtStrategy],
+    controllers: [AuthController],
+    providers: [AuthService, AccessJwtStrategy, RefreshJwtStrategy],
     exports: [AuthService],
 })
 export class AuthModule {}
