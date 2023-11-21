837b15ffb967e7a2e3ed712a038e7e34e7706143
ootr47 <ootr45@gmail.com>
2023-11-22 01:06:03 +0900

    feat: settingInfo 데이터 및 이름 변경

---
+++ android/app/src/main/java/app/priceguard/ui/home/mypage/SettingItemInfo.kt
@@ -0,0 +1,15 @@
+package app.priceguard.ui.home.mypage
+
+import android.graphics.drawable.Drawable
+
+data class SettingItemInfo(
+    val id: Setting,
+    val icon: Drawable?,
+    val title: String
+)
+
+enum class Setting {
+    NOTIFICATION,
+    THEME,
+    LOGOUT
+}
