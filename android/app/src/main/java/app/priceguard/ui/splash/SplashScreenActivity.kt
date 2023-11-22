00ec81f7eff104aa6e4563faded04bb37dbca006
Taewan Park <swimingtw@gmail.com>
2023-11-22 10:00:34 +0900

    feat: SplashScreen API를 통해 Splash Screen 구현

--- android/app/src/main/java/app/priceguard/ui/splash/SplashScreenActivity.kt
+++ android/app/src/main/java/app/priceguard/ui/splash/SplashScreenActivity.kt
@@ -5,6 +5,8 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
@@ -15,6 +17,7 @@ import app.priceguard.ui.intro.IntroActivity
 import app.priceguard.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
@@ -24,27 +27,73 @@ class SplashScreenActivity : AppCompatActivity() {
     @Inject
     lateinit var tokenRepository: TokenRepository
     private lateinit var binding: ActivitySplashScreenBinding
+    private var isReady = false
+    var accessToken: String? = null
+    var refreshToken: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
 
         lifecycleScope.launch {
-            val accessToken = tokenRepository.getAccessToken()
-            val refreshToken = tokenRepository.getRefreshToken()
+            accessToken = tokenRepository.getAccessToken()
+            refreshToken = tokenRepository.getRefreshToken()
+            isReady = true
 
-            if (accessToken == null || refreshToken == null) {
-                // Intro Activity로 이동
-                tokenRepository.clearTokens()
-                launchActivityAndExit(this@SplashScreenActivity, IntroActivity::class.java)
-                return@launch
+            if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.S) {
+                useActivitySplashScreen()
             }
-
-            // TODO: Token 갱신하기
-
-            // Main Activity로 이동
-            launchActivityAndExit(this@SplashScreenActivity, MainActivity::class.java)
         }
+
+        val content: View = findViewById(android.R.id.content)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            content.viewTreeObserver.addOnPreDrawListener(onPreDrawListener(content))
+        }
+    }
+
+    private suspend fun useActivitySplashScreen() {
+        if (accessToken == null || refreshToken == null) {
+            // Intro Activity로 이동
+            tokenRepository.clearTokens()
+            launchActivityAndExit(this@SplashScreenActivity, IntroActivity::class.java)
+            return
+        }
+
+        // TODO: Token 갱신하기
+
+        // Main Activity로 이동
+        launchActivityAndExit(this@SplashScreenActivity, MainActivity::class.java)
+    }
+
+    private fun onPreDrawListener(content: View) =
+        object : ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                if (!isReady) {
+                    return false
+                }
+
+                if (accessToken == null || refreshToken == null) {
+                    return directToIntro(content)
+                }
+
+                return directToMain(content)
+            }
+        }
+
+    private fun ViewTreeObserver.OnPreDrawListener.directToIntro(content: View): Boolean {
+        lifecycleScope.launch {
+            tokenRepository.clearTokens()
+        }
+        launchActivityAndExit(this@SplashScreenActivity, IntroActivity::class.java)
+        content.viewTreeObserver.removeOnPreDrawListener(this)
+        return true
+    }
+
+    private fun ViewTreeObserver.OnPreDrawListener.directToMain(content: View): Boolean {
+        launchActivityAndExit(this@SplashScreenActivity, MainActivity::class.java)
+        content.viewTreeObserver.removeOnPreDrawListener(this)
+        return true
     }
 
     private fun <T : Activity> launchActivityAndExit(context: Context, clazz: Class<T>) {
