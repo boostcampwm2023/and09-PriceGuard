956fbf9f81a9c31aa5454882c8d76dd650344fb7
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-22 13:38:30 +0900

    feat: URL 검증 API 라우트 핸들러 작성

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ProductUrlDto } from '../dto/product.url.dto';
 import { ProductDto } from '../dto/product.dto';
+import { ProductDetailsDto } from 'src/dto/product.details.dto';
 
 @Injectable()
 export class ProductService {
-    verifyUrl(productUrlDto: ProductUrlDto) {
+    async verifyUrl(productUrlDto: ProductUrlDto): Promise<ProductDetailsDto> {
         console.log(productUrlDto);
     }
 
