bb3b795376610c8db2d5410fcf8531ad76ea898d
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-22 16:13:38 +0900

    feat: URL 검증 API 서비스 로직 작성

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,12 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ProductUrlDto } from '../dto/product.url.dto';
 import { ProductDto } from '../dto/product.dto';
 import { ProductDetailsDto } from 'src/dto/product.details.dto';
-
+import axios from 'axios';
+import { openApiUrl11st, xmlParse11st } from 'src/utils/openapi.11st';
+const SHOP_URL_11ST = 'www.11st.co.kr';
 @Injectable()
 export class ProductService {
     async verifyUrl(productUrlDto: ProductUrlDto): Promise<ProductDetailsDto> {
-        console.log(productUrlDto);
+        const { productUrl } = productUrlDto;
+        const { hostname, pathname } = new URL(productUrl);
+        const [, keyword, code] = pathname.split('/');
+        if (hostname !== SHOP_URL_11ST || keyword !== 'products')
+            throw new HttpException('URL이 유효하지 않습니다.', HttpStatus.BAD_REQUEST);
+        const openApiUrl = openApiUrl11st(code);
+        const xml = await axios.get(openApiUrl, { responseType: 'arraybuffer' });
+        const productDetails = xmlParse11st(xml.data);
+        const productCode = productDetails['ProductCode']['text'];
+        const productName = productDetails['ProductName']['text'];
+        const shop = '11번가';
+        const imageUrl = productDetails['BasicImage']['text'];
+        return { productCode, productName, shop, imageUrl };
     }
 
     addProduct(productDto: ProductDto) {
