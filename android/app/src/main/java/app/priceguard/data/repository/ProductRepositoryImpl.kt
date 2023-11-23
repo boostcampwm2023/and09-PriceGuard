962684249813e07bd1da068477ba5feec12ceeff
EunhoKang <zay0njugin@gmail.com>
2023-11-23 11:40:00 +0900

    feat: 레포지토리 구현 변경

--- android/app/src/main/java/app/priceguard/data/repository/ProductRepositoryImpl.kt
+++ android/app/src/main/java/app/priceguard/data/repository/ProductRepositoryImpl.kt
@@ -1,13 +1,18 @@
 package app.priceguard.data.repository
 
 import app.priceguard.data.dto.ProductAddRequest
-import app.priceguard.data.dto.ProductListResponse
+import app.priceguard.data.dto.ProductData
+import app.priceguard.data.dto.ProductListResult
+import app.priceguard.data.dto.ProductListState
 import app.priceguard.data.dto.ProductResponse
 import app.priceguard.data.dto.ProductVerifyRequest
+import app.priceguard.data.network.APIResult
 import app.priceguard.data.network.ProductAPI
+import app.priceguard.data.network.getApiResult
 import javax.inject.Inject
 
-class ProductRepositoryImpl @Inject constructor(private val productAPI: ProductAPI) : ProductRepository {
+class ProductRepositoryImpl @Inject constructor(private val productAPI: ProductAPI) :
+    ProductRepository {
 
     override suspend fun verifyLink(productUrl: ProductVerifyRequest): ProductResponse {
         TODO("Not yet implemented")
@@ -17,11 +22,44 @@ class ProductRepositoryImpl @Inject constructor(private val productAPI: ProductA
         TODO("Not yet implemented")
     }
 
-    override suspend fun getProductList(): ProductListResponse {
-        TODO("Not yet implemented")
+    override suspend fun getProductList(): ProductListResult {
+        val response = getApiResult {
+            productAPI.getProductList()
+        }
+        when (response) {
+            is APIResult.Success -> {
+                return ProductListResult(
+                    ProductListState.SUCCESS,
+                    response.data.trackingList?.map { dto ->
+                        ProductData(
+                            dto.productName ?: "",
+                            dto.productCode ?: "",
+                            dto.shop ?: "",
+                            dto.imageUrl ?: ""
+                        )
+                    } ?: listOf()
+                )
+            }
+
+            is APIResult.Error -> {
+                return when (response.code) {
+                    401 -> {
+                        ProductListResult(ProductListState.PERMISSION_DENIED, listOf())
+                    }
+
+                    404 -> {
+                        ProductListResult(ProductListState.NOT_FOUND, listOf())
+                    }
+
+                    else -> {
+                        ProductListResult(ProductListState.UNDEFINED_ERROR, listOf())
+                    }
+                }
+            }
+        }
     }
 
-    override suspend fun getRecommendedProductList(): ProductListResponse {
+    override suspend fun getRecommendedProductList(): ProductListResult {
         TODO("Not yet implemented")
     }
 
