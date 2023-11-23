466ed0ee67572b0ca2791143aaef521c5aaa39a9
EunhoKang <zay0njugin@gmail.com>
2023-11-23 11:39:27 +0900

    feat: 목록 화면 표시 관련 Data 클래스 추가 및 변경

--- android/app/src/main/java/app/priceguard/data/repository/ProductRepository.kt
+++ android/app/src/main/java/app/priceguard/data/repository/ProductRepository.kt
@@ -1,7 +1,7 @@
 package app.priceguard.data.repository
 
 import app.priceguard.data.dto.ProductAddRequest
-import app.priceguard.data.dto.ProductListResponse
+import app.priceguard.data.dto.ProductListResult
 import app.priceguard.data.dto.ProductResponse
 import app.priceguard.data.dto.ProductVerifyRequest
 
@@ -11,9 +11,9 @@ interface ProductRepository {
 
     suspend fun addProduct(productAddRequest: ProductAddRequest): ProductResponse
 
-    suspend fun getProductList(): ProductListResponse
+    suspend fun getProductList(): ProductListResult
 
-    suspend fun getRecommendedProductList(): ProductListResponse
+    suspend fun getRecommendedProductList(): ProductListResult
 
     suspend fun getProductDetail(productCode: String): ProductResponse
 
