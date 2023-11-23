d571ec92959dd14cf76ea7e7014d68719e972ee3
Taewan Park <swimingtw@gmail.com>
2023-11-23 22:48:04 +0900

    fix: 성공시에만 존재하는 값들을 Optional 타입으로 설정

--- android/app/src/main/java/app/priceguard/data/dto/ProductResponse.kt
+++ android/app/src/main/java/app/priceguard/data/dto/ProductResponse.kt
@@ -6,15 +6,15 @@ import kotlinx.serialization.Serializable
 data class ProductResponse(
     val statusCode: Int,
     val message: String,
-    val productName: String?,
-    val productCode: String?,
-    val shop: String?,
-    val imageUrl: String?,
-    val shopUrl: String?,
-    val targetPrice: Int?,
-    val lowestPrice: Int?,
-    val price: Int?
+    val productName: String? = null,
+    val productCode: String? = null,
+    val shop: String? = null,
+    val imageUrl: String? = null,
     val rank: Int? = null,
+    val shopUrl: String? = null,
+    val targetPrice: Int? = null,
+    val lowestPrice: Int? = null,
+    val price: Int? = null
 )
 
 data class ProductDetailResult(
