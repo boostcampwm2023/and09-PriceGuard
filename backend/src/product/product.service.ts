a35d1c43abdb5dbb67c9c414b625d9c31bf5ecf4
Muungi <mks4010@naver.com>
2023-11-23 23:50:55 +0900

    feat: 상세정보에 현재 상품 랭킹 반영

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -69,7 +69,7 @@ export class ProductService {
     }
 
     async getRecommendList() {
-        const recommendList = await this.trackingProductRepository.getRankingList();
+        const recommendList = await this.trackingProductRepository.getTotalInfoRankingList();
         const recommendListInfo = recommendList.map((product, index) => {
             const { productName, productCode, shop, imageUrl } = product;
             return {
@@ -94,12 +94,15 @@ export class ProductService {
         const trackingProduct = await this.trackingProductRepository.findOne({
             where: { userId: userId, productId: selectProduct.id },
         });
+        const ranklist = await this.trackingProductRepository.getRankingList();
+        const rank = ranklist.findIndex((product) => product.productId === selectProduct.id) + 1;
+        console.log(ranklist, selectProduct.id, rank);
         /* 랭킹, 역대 최저가, 현재가격은 더미 데이터 사용 중, 그래프 데이터 추가 필요 */
         return {
             productName: selectProduct.productName,
             shop: selectProduct.shop,
             imageUrl: selectProduct.imageUrl,
-            rank: 1,
+            rank: rank,
             shopUrl: selectProduct.shopUrl,
             targetPrice: trackingProduct ? trackingProduct.targetPrice : -1,
             lowestPrice: 500,
