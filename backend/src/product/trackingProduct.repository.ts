653ac95a8cde5d5652e365f1459816f883c534e7
Muungi <mks4010@naver.com>
2023-11-23 21:48:56 +0900

    feat: product 테이블 정보 함께 반환

--- backend/src/product/trackingProduct.repository.ts
+++ backend/src/product/trackingProduct.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TrackingProduct } from 'src/entities/trackingProduct.entity';
+import { Product } from 'src/entities/product.entity';
 
 @Injectable()
 export class TrackingProductRepository extends Repository<TrackingProduct> {
@@ -21,9 +22,17 @@ export class TrackingProductRepository extends Repository<TrackingProduct> {
     async getRankigList() {
         const recommendList = await this.repository
             .createQueryBuilder('tracking_product')
-            .select('tracking_product.productId as productId')
+            .select([
+                'tracking_product.productId as productId',
+                'COUNT(tracking_product.userId) as userCount',
+                'product.productName as productName',
+                'product.productCode as productCode',
+                'product.shop as shop',
+                'product.imageUrl as imageUrl',
+            ])
+            .leftJoin(Product, 'product', 'tracking_product.productId = product.id')
             .groupBy('tracking_product.productId')
-            .orderBy('COUNT(tracking_product.userId)', 'DESC')
+            .orderBy('userCount', 'DESC')
             .getRawMany();
         return recommendList;
     }
