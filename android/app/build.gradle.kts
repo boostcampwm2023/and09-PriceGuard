e15ba5ebb9f65549db6b8b0a93239e2d28ee1e5b
Taewan Park <swimingtw@gmail.com>
2023-11-24 23:05:56 +0900

    feat: Android 앱 버전 업그레이드

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "app.priceguard"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
