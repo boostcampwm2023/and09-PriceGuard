61a70d9c8de0f1dc4912c39309e294227c9966e8
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-28 23:11:53 +0900

    feat: 한국 timezone 설정
    Co-authored-by: Muungi <mks4010@naver.com>

--- backend/src/schema/product.schema.ts
+++ backend/src/schema/product.schema.ts
@@ -1,15 +1,21 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { HydratedDocument } from 'mongoose';
+import mongoose from 'mongoose';
 
-export type CatDocument = HydratedDocument<ProductPrice>;
+const KR_OFFSET = 9 * 60 * 60 * 1000;
 
-@Schema()
+@Schema({
+    timestamps: {
+        createdAt: 'time',
+        currentTime: () => new Date(+new Date() + KR_OFFSET),
+        updatedAt: false,
+    },
+})
 export class ProductPrice {
     @Prop()
     productId: string;
 
-    @Prop({ default: new Date(), type: mongoose.Schema.Types.Date })
-    time: string;
+    @Prop({ type: mongoose.Schema.Types.Date })
+    time: Date;
 
     @Prop()
     price: number;
