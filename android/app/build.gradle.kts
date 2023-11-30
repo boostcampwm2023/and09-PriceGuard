4ed5a8ddfdb95f4d380d44ae60c0abb0dd4f20be
ootr47 <ootr45@gmail.com>
2023-12-01 02:40:00 +0900

    feat:  버전 업그레이드

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "app.priceguard"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
