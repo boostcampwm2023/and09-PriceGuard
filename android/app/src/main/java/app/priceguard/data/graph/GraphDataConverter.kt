fb02d87382f22366bdd3d770b98eb1882cd80ac5
EunhoKang <zay0njugin@gmail.com>
2023-12-01 01:38:15 +0900

    fix: 현재 시간보다 빠른 데이터는 무시

--- android/app/src/main/java/app/priceguard/data/graph/GraphDataConverter.kt
+++ android/app/src/main/java/app/priceguard/data/graph/GraphDataConverter.kt
@@ -18,9 +18,7 @@ class GraphDataConverter @Inject constructor() {
             dataList.add(ProductChartData(dto.time / 1000, dto.price.toFloat(), dto.isSoldOut.not()))
         }
         val currentTime = (System.currentTimeMillis() / 1000).toFloat()
-        if (dataList.last().x < currentTime) {
-            dataList.add(ProductChartData(currentTime, dataList.last().y, dataList.last().valid))
-        }
-        return dataList.toList().sortedBy { it.x }
+        dataList.add(ProductChartData(currentTime, dataList.last().y, dataList.last().valid))
+        return dataList.toList().sortedBy { it.x }.filter { it.x <= currentTime }
     }
 }
