1e26570edad44005ab1da0555a250ffec632a619
ootr47 <ootr45@gmail.com>
2023-12-05 01:53:47 +0900

    refactor: sharedPreference -> datastore로 migration

--- android/app/src/main/java/app/priceguard/ui/PriceGuardApp.kt
+++ android/app/src/main/java/app/priceguard/ui/PriceGuardApp.kt
@@ -4,46 +4,57 @@ import android.app.Application
 import android.app.UiModeManager
 import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
+import app.priceguard.data.datastore.ConfigDataSource
 import app.priceguard.ui.home.theme.ThemeDialogFragment
 import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
 class PriceGuardApp : Application() {
+
+    @Inject
+    lateinit var configDataSource: ConfigDataSource
 
     override fun onCreate() {
         super.onCreate()
 
         initAppTheme()
     }
+
     private fun initAppTheme() {
-        val sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        CoroutineScope(Dispatchers.IO).launch {
+            val dynamicColorMode = configDataSource.getDynamicMode()
+            val darkMode = configDataSource.getDarkMode()
 
-        val dynamicColorMode = sharedPreferences.getInt("DynamicColor", ThemeDialogFragment.MODE_DYNAMIC_NO)
-        val darkMode = sharedPreferences.getInt("DarkMode", ThemeDialogFragment.MODE_SYSTEM)
-
-        when (dynamicColorMode) {
-            ThemeDialogFragment.MODE_DYNAMIC -> {
-                DynamicColors.applyToActivitiesIfAvailable(this)
-            }
-        }
-
-        when (darkMode) {
-            ThemeDialogFragment.MODE_LIGHT -> {
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-                    val uiModeManager = getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
-                    uiModeManager.setApplicationNightMode(UiModeManager.MODE_NIGHT_NO)
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            when (dynamicColorMode) {
+                ThemeDialogFragment.MODE_DYNAMIC -> {
+                    DynamicColors.applyToActivitiesIfAvailable(this@PriceGuardApp)
                 }
             }
 
-            ThemeDialogFragment.MODE_DARK -> {
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-                    val uiModeManager = getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
-                    uiModeManager.setApplicationNightMode(UiModeManager.MODE_NIGHT_YES)
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            when (darkMode) {
+                ThemeDialogFragment.MODE_LIGHT -> {
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+                        val uiModeManager =
+                            getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+                        uiModeManager.setApplicationNightMode(UiModeManager.MODE_NIGHT_NO)
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    }
+                }
+
+                ThemeDialogFragment.MODE_DARK -> {
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+                        val uiModeManager =
+                            getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+                        uiModeManager.setApplicationNightMode(UiModeManager.MODE_NIGHT_YES)
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
                 }
             }
         }
--- android/app/src/main/java/app/priceguard/ui/home/theme/ThemeDialogFragment.kt
+++ android/app/src/main/java/app/priceguard/ui/home/theme/ThemeDialogFragment.kt
@@ -1,17 +1,27 @@
 package app.priceguard.ui.home.theme
 
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.lifecycleScope
 import app.priceguard.R
+import app.priceguard.data.datastore.ConfigDataSource
 import app.priceguard.databinding.FragmentThemeDialogBinding
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.color.DynamicColorsOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
+@AndroidEntryPoint
 class ThemeDialogFragment : DialogFragment() {
+
+    @Inject
+    lateinit var configDataSource: ConfigDataSource
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val binding: FragmentThemeDialogBinding =
@@ -64,57 +74,54 @@ class ThemeDialogFragment : DialogFragment() {
                         MODE_SYSTEM
                     }
                 }
-                saveModeWithPreference(dynamicMode, darkMode)
-
+                saveTheme(dynamicMode, darkMode)
                 dismiss()
             }
         }.create()
     }
 
-    private fun saveModeWithPreference(dynamicMode: Int, darkMode: Int) {
-        val sharedPreferences =
-            requireActivity().getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
-
-        val editor = sharedPreferences.edit()
-        editor.putInt("DynamicColor", dynamicMode)
-        editor.putInt("DarkMode", darkMode)
-        editor.apply()
+    private fun saveTheme(dynamicMode: Int, darkMode: Int) {
+        this.lifecycleScope.launch(Dispatchers.IO) {
+            configDataSource.saveDynamicMode(dynamicMode)
+            configDataSource.saveDarkMode(darkMode)
+        }
     }
 
     private fun setCheckedButton(binding: FragmentThemeDialogBinding) {
-        val sharedPreferences =
-            requireActivity().getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        lifecycleScope.launch {
+            val dynamicColorMode = configDataSource.getDynamicMode()
+            val darkMode = configDataSource.getDarkMode()
 
-        val dynamicColorMode = sharedPreferences.getInt("DynamicColor", MODE_DYNAMIC_NO)
-        val darkMode = sharedPreferences.getInt("DarkMode", MODE_SYSTEM)
+            withContext(Dispatchers.Main) {
+                binding.rgDynamicColor.check(
+                    when (dynamicColorMode) {
+                        MODE_DYNAMIC -> {
+                            R.id.rb_yes
+                        }
 
-        binding.rgDynamicColor.check(
-            when (dynamicColorMode) {
-                MODE_DYNAMIC -> {
-                    R.id.rb_yes
-                }
+                        else -> {
+                            R.id.rb_no
+                        }
+                    }
+                )
 
-                else -> {
-                    R.id.rb_no
-                }
+                binding.rgDarkMode.check(
+                    when (darkMode) {
+                        MODE_LIGHT -> {
+                            R.id.rb_light
+                        }
+
+                        MODE_DARK -> {
+                            R.id.rb_dark
+                        }
+
+                        else -> {
+                            R.id.rb_system
+                        }
+                    }
+                )
             }
-        )
-
-        binding.rgDarkMode.check(
-            when (darkMode) {
-                MODE_LIGHT -> {
-                    R.id.rb_light
-                }
-
-                MODE_DARK -> {
-                    R.id.rb_dark
-                }
-
-                else -> {
-                    R.id.rb_system
-                }
-            }
-        )
+        }
     }
 
     companion object {
