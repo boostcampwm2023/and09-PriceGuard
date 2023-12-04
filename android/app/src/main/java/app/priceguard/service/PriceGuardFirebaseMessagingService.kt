b13db3ceda7a98b5e84c50750fffad296ad3b4c2
Taewan Park <swimingtw@gmail.com>
2023-12-05 02:59:10 +0900

    feat: FirebaseMessagingService 뼈대 추가

--- android/app/src/main/java/app/priceguard/service/PriceGuardFirebaseMessagingService.kt
+++ android/app/src/main/java/app/priceguard/service/PriceGuardFirebaseMessagingService.kt
@@ -1,5 +1,15 @@
 package app.priceguard.service
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 
-class PriceGuardFirebaseMessagingService : FirebaseMessagingService()
+class PriceGuardFirebaseMessagingService : FirebaseMessagingService() {
+    // Init 시에도 호출됨
+    override fun onNewToken(token: String) {
+        Log.d("PriceGuardFirebaseMessagingService", "Refreshed token: $token")
+
+        /* TODO: If you want to send messages to this application instance
+            or manage this apps subscriptions on the server side,
+            send the FCM registration token to your app server. */
+    }
+}
