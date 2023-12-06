2660935749ef5b40fefc1a03f21b35c5e3c03f0f
EunhoKang <zay0njugin@gmail.com>
2023-12-06 11:48:10 +0900

    refactor: 제네릭 타입 컨벤션

--- android/app/src/main/java/app/priceguard/data/repository/RepositoryResult.kt
+++ android/app/src/main/java/app/priceguard/data/repository/RepositoryResult.kt
@@ -1,8 +1,8 @@
 package app.priceguard.data.repository
 
-sealed class RepositoryResult<out T, out R> {
+sealed class RepositoryResult<out T, out S> {
 
     data class Success<out T>(val data: T) : RepositoryResult<T, Nothing>()
 
-    data class Error<out R>(val errorState: R) : RepositoryResult<Nothing, R>()
+    data class Error<out S>(val errorState: S) : RepositoryResult<Nothing, S>()
 }
