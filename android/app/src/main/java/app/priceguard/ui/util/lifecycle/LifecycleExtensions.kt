ab6be2f054bbf8aa371089189afe8f13c25ef44f
ootr47 <ootr45@gmail.com>
2023-12-07 00:57:55 +0900

    fix: start destination 프래그먼트에서 event collect 중첩 현상 수정

--- android/app/src/main/java/app/priceguard/ui/util/lifecycle/LifecycleExtensions.kt
+++ android/app/src/main/java/app/priceguard/ui/util/lifecycle/LifecycleExtensions.kt
@@ -5,10 +5,11 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
-    lifecycleScope.launch {
+fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit): Job {
+    return lifecycleScope.launch {
         repeatOnLifecycle(Lifecycle.State.STARTED, block)
     }
 }
