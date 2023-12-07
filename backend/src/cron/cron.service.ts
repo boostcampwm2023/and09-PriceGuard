18ecf75b30a42386ccd68fa083214c7b0d355e28
ByeongIk Choi <bichoi0715@gmail.com>
2023-12-07 20:26:18 +0900

    refactor: 함수 위치 변경

--- backend/src/cron/cron.service.ts
+++ backend/src/cron/cron.service.ts
@@ -15,8 +15,6 @@ import { TrackingProduct } from 'src/entities/trackingProduct.entity';
 import Redis from 'ioredis';
 import { InjectRedis } from '@songkeys/nestjs-redis';
 
-const isDefined = <T>(x: T | undefined): x is T => x !== undefined;
-
 @Injectable()
 export class CronService {
     constructor(
@@ -29,6 +27,27 @@ export class CronService {
         @InjectRedis() private readonly redis: Redis,
         private readonly firebaseService: FirebaseService,
     ) {}
+
+    private isDefined = <T>(x: T | undefined): x is T => x !== undefined;
+
+    @Cron('* */10 * * * *')
+    async cyclicPriceChecker() {
+        const totalProducts = await this.productRepository.find({ select: { id: true, productCode: true } });
+        const recentProductInfo = await Promise.all(
+            totalProducts.map(({ productCode, id }) => getProductInfo11st(productCode, id)),
+        );
+        const checkProducts = await Promise.all(recentProductInfo.map((data) => this.getUpdatedProduct(data)));
+        const updatedProducts = checkProducts.filter(this.isDefined);
+        if (updatedProducts.length > 0) {
+            await this.productPriceModel.insertMany(
+                updatedProducts.map(({ productId, productPrice, isSoldOut }) => {
+                    return { productId, price: productPrice, isSoldOut };
+                }),
+            );
+            await this.pushNotifications(updatedProducts);
+        }
+    }
+
     async getUpdatedProduct(data: ProductInfoDto) {
         const { productId, productPrice, isSoldOut } = data;
         const cacheData = await this.redis.get(`product:${productId}`);
@@ -46,23 +65,6 @@ export class CronService {
             return data;
         }
     }
-    @Cron('* */10 * * * *')
-    async cyclicPriceChecker() {
-        const totalProducts = await this.productRepository.find({ select: { id: true, productCode: true } });
-        const recentProductInfo = await Promise.all(
-            totalProducts.map(({ productCode, id }) => getProductInfo11st(productCode, id)),
-        );
-        const checkProducts = await Promise.all(recentProductInfo.map((data) => this.getUpdatedProduct(data)));
-        const updatedProducts = checkProducts.filter(isDefined);
-        if (updatedProducts.length > 0) {
-            await this.productPriceModel.insertMany(
-                updatedProducts.map(({ productId, productPrice, isSoldOut }) => {
-                    return { productId, price: productPrice, isSoldOut };
-                }),
-            );
-            await this.pushNotifications(updatedProducts);
-        }
-    }
 
     async pushNotifications(updatedProducts: ProductInfoDto[]) {
         const { messages, products } = await this.getNotifications(updatedProducts);
@@ -76,52 +78,6 @@ export class CronService {
         if (successProducts.length > 0) {
             await this.trackingProductRepository.save(successProducts);
         }
-    }
-    private getMessage(
-        productCode: string,
-        productName: string,
-        productPrice: number,
-        imageUrl: string,
-        token: string,
-    ): Message {
-        return {
-            notification: {
-                title: '목표 가격 이하로 내려갔습니다!',
-                body: `${productName}의 현재 가격은 ${productPrice}원 입니다.`,
-            },
-            data: {
-                productCode,
-            },
-            android: {
-                notification: {
-                    channelId: CHANNEL_ID,
-                    imageUrl,
-                },
-            },
-            token,
-        };
-    }
-    async findMatchedProducts(trackingList: TrackingProduct[], product: ProductInfoDto) {
-        const { productPrice, productCode, productName, imageUrl } = product;
-        const notifications = [];
-        const matchedProducts = [];
-
-        for (const trackingProduct of trackingList) {
-            const { userId, targetPrice, isFirst, isAlert } = trackingProduct;
-            if (!isFirst && targetPrice < productPrice) {
-                trackingProduct.isFirst = true;
-                await this.trackingProductRepository.save(trackingProduct);
-            } else if (targetPrice >= productPrice && isFirst && isAlert) {
-                const firebaseToken = await this.redis.get(`firebaseToken:${userId}`);
-                if (firebaseToken) {
-                    notifications.push(
-                        this.getMessage(productCode, productName, productPrice, imageUrl, firebaseToken),
-                    );
-                    matchedProducts.push(trackingProduct);
-                }
-            }
-        }
-        return { notifications, matchedProducts };
     }
     async getNotifications(
         productInfo: ProductInfoDto[],
@@ -151,6 +107,54 @@ export class CronService {
         return {
             messages: allNotifications,
             products: allMatchedProducts,
+        };
+    }
+
+    async findMatchedProducts(trackingList: TrackingProduct[], product: ProductInfoDto) {
+        const { productPrice, productCode, productName, imageUrl } = product;
+        const notifications = [];
+        const matchedProducts = [];
+
+        for (const trackingProduct of trackingList) {
+            const { userId, targetPrice, isFirst, isAlert } = trackingProduct;
+            if (!isFirst && targetPrice < productPrice) {
+                trackingProduct.isFirst = true;
+                await this.trackingProductRepository.save(trackingProduct);
+            } else if (targetPrice >= productPrice && isFirst && isAlert) {
+                const firebaseToken = await this.redis.get(`firebaseToken:${userId}`);
+                if (firebaseToken) {
+                    notifications.push(
+                        this.getMessage(productCode, productName, productPrice, imageUrl, firebaseToken),
+                    );
+                    matchedProducts.push(trackingProduct);
+                }
+            }
+        }
+        return { notifications, matchedProducts };
+    }
+
+    private getMessage(
+        productCode: string,
+        productName: string,
+        productPrice: number,
+        imageUrl: string,
+        token: string,
+    ): Message {
+        return {
+            notification: {
+                title: '목표 가격 이하로 내려갔습니다!',
+                body: `${productName}의 현재 가격은 ${productPrice}원 입니다.`,
+            },
+            data: {
+                productCode,
+            },
+            android: {
+                notification: {
+                    channelId: CHANNEL_ID,
+                    imageUrl,
+                },
+            },
+            token,
         };
     }
 }
