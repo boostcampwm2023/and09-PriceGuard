26e21a96a2615a46c8f2a733fcda498823109631
ootr47 <ootr45@gmail.com>
2024-01-05 18:01:52 +0900

    refactor: 미사용 함수 제거

--- android/app/src/main/java/app/priceguard/ui/slider/util.kt
+++ android/app/src/main/java/app/priceguard/ui/slider/util.kt
@@ -11,47 +11,7 @@ fun Px.toDp(context: Context): Dp {
     return Dp(value / density)
 }
 
-operator fun Px.plus(other: Px): Px {
-    return Px(value + other.value)
-}
-
-operator fun Px.minus(other: Px): Px {
-    return Px(value - other.value)
-}
-
-operator fun Px.times(other: Px): Px {
-    return Px(value * other.value)
-}
-
-operator fun Px.div(other: Px): Px {
-    return Px(value / other.value)
-}
-
-operator fun Px.rem(other: Px): Px {
-    return Px(value % other.value)
-}
-
 fun Dp.toPx(context: Context): Px {
     val density = context.resources.displayMetrics.density
     return Px(value * density)
-}
-
-operator fun Dp.plus(other: Dp): Dp {
-    return Dp(value + other.value)
-}
-
-operator fun Dp.minus(other: Dp): Dp {
-    return Dp(value - other.value)
-}
-
-operator fun Dp.times(other: Dp): Dp {
-    return Dp(value * other.value)
-}
-
-operator fun Dp.div(other: Dp): Dp {
-    return Dp(value / other.value)
-}
-
-operator fun Dp.rem(other: Dp): Dp {
-    return Dp(value % other.value)
 }
