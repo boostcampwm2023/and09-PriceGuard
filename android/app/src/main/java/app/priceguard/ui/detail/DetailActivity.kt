9b04600a881f25fb31caa325c80de4bf04edb29e
EunhoKang <zay0njugin@gmail.com>
2024-02-06 00:37:43 +0900

    fix: 사용되지 않는 import문 삭제

--- android/app/src/main/java/app/priceguard/ui/detail/DetailActivity.kt
+++ android/app/src/main/java/app/priceguard/ui/detail/DetailActivity.kt
@@ -4,7 +4,6 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.activity.result.contract.ActivityResultContracts
