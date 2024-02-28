3da7dc54487321adfa5773d3b2ea81c86a13bc1a
ootr47 <ootr45@gmail.com>
2024-02-28 18:57:02 +0900

    fix: 이메일 검증 Request, Response @Serializable 추가

--- android/app/src/main/java/app/priceguard/data/dto/verifyemail/VerifyEmailRequest.kt
+++ android/app/src/main/java/app/priceguard/data/dto/verifyemail/VerifyEmailRequest.kt
@@ -1,5 +1,8 @@
 package app.priceguard.data.dto.verifyemail
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class VerifyEmailRequest(
     val email: String,
     val verificationCode: String
--- android/app/src/main/java/app/priceguard/data/dto/verifyemail/VerifyEmailResponse.kt
+++ android/app/src/main/java/app/priceguard/data/dto/verifyemail/VerifyEmailResponse.kt
@@ -1,5 +1,8 @@
 package app.priceguard.data.dto.verifyemail
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class VerifyEmailResponse(
     val statusCode: Int,
     val message: String,
