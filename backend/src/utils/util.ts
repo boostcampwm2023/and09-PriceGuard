6eaf4ab808abf7653f178c129bdba0c160a34ffa
Muungi <mks4010@naver.com>
2024-05-27 21:37:23 +0900

    feat: smartstore crawling useragent 설정 변경

--- backend/src/utils/util.ts
+++ backend/src/utils/util.ts
@@ -1,3 +1,5 @@
+import * as randomUseragent from 'random-useragent';
+
 export function createRandomNumber(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -8,4 +10,25 @@ export function getSecondsUntilMidnight() {
     midnight.setHours(24, 0, 0, 0);
     const secondsUntilMidnight = Math.floor((midnight.getTime() - now.getTime()) / 1000);
     return secondsUntilMidnight;
+}
+
+function isVersionInRange(version: string) {
+    const numericVersion = parseFloat(version);
+    return (numericVersion >= 20 && numericVersion < 29) || (numericVersion >= 535 && numericVersion < 538);
+}
+
+function isOsNameValid(osName: string) {
+    return osName === 'Windows' || osName === 'Mac OS';
+}
+
+export function getRandomUserAgent() {
+    const userAgent = randomUseragent.getRandom(function (ua) {
+        return (
+            ua.userAgent.includes('AppleWebKit') &&
+            ua.browserName === 'Chrome' &&
+            isOsNameValid(ua.osName) &&
+            isVersionInRange(ua.engineVersion)
+        );
+    });
+    return userAgent;
 }
